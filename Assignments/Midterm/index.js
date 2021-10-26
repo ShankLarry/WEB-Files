8f8ba385d46bb2f70205aef45a90aba0e8616473
Larry Ditton <70234897+ShankLarry@users.noreply.github.com>
2021-10-26 13:07:33 -0600

    Modified img alt and formating

--- Assignments/Midterm/index.js
+++ Assignments/Midterm/index.js
@@ -1,9 +1,24 @@
-let mornings = ["Light a Candle", "Get some exercise", "Call a friend", "Watch something funny", "Get some exercise", "Check your list of goals", "Play a game", "Take a nap", "Go to a party", "Review your week", "Sleep in", "Watch something you enjoy", "Go for a walk", "Review week ahead"];
-let afternoons = ["Sit quietly for 5 minutes", "Meditate", "Go for a walk", "Do some laundry", "Journal", "Make a plan with a friend", "List 3 goals for the week"];
-let evenings = ["Eat a healthy snack", "Go to bed early", "Stretch", "Think of three things you're grateful for", "Consider a fun future plan", "Listen to music", "Make a comfort meal", "Call your family", "Help a stranger", "Take an evening shower", "Eat a healthy lunch", "Put away your phone for an hour", "Eat a healthy meal", "Lay out clothes for tomorrow"];
+let mornings = ["Light a Candle", "Get some exercise", "Call a friend",
+  "Watch something funny", "Get some exercise", "Check your list of goals",
+  "Play a game", "Take a nap", "Go to a party", "Review your week", "Sleep in",
+  "Watch something you enjoy", "Go for a walk", "Review week ahead"];
+
+let afternoons = ["Sit quietly for 5 minutes", "Meditate", "Go for a walk",
+  "Do some laundry", "Journal", "Make a plan with a friend",
+  "List 3 goals for the week"];
+
+let evenings = ["Eat a healthy snack", "Go to bed early", "Stretch",
+"Think of three things you're grateful for", "Consider a fun future plan",
+"Listen to music", "Make a comfort meal", "Call your family", "Help a stranger",
+"Take an evening shower", "Eat a healthy lunch",
+"Put away your phone for an hour", "Eat a healthy meal",
+"Lay out clothes for tomorrow"];
+
 let tags = ["first", "second", "third", "fourth", "fifth"];
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let images = ["cookie_love.jpg", "hike.jpg", "meditate.jpg", "sleep.jpg", "tea.jpg", "yoga.jpg", "weights.jpg"];
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
+  "Friday", "Saturday"];
+let images = ["cookie_love.jpg", "hike.jpg", "meditate.jpg", "sleep.jpg",
+"tea.jpg", "yoga.jpg", "weights.jpg"];
 let idea = 0;
 let aftIdea = 0;
 let numDays = 0;
@@ -26,7 +41,8 @@ function dayClicked(){
   thisDay = this.id;
 
   //Change the picture when button clicked:
-  document.getElementById("imagery").innerHTML = "<img src = 'assets/" + images[numDays] +"'>";
+  document.getElementById("imagery").innerHTML =
+    "<img alt='Drawing of self-care activity' src = 'assets/" + images[numDays] +"'>";
   if(numDays>=6){
     numDays=0;
   }
